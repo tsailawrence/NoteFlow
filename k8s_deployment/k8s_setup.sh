2740ae7e7affd47d33ebb4251606ffd1feaa66c6
r11725042 <r11725042@ntu.edu.tw>
2023-05-23 15:01:09 +0800

    k8s revision

--- k8s_deployment/k8s_setup.sh
+++ k8s_deployment/k8s_setup.sh
@@ -1,7 +1,7 @@
 source k8s_env.sh
-envsubst < k8s-configMap.yml > k8s-configMap.yml
-envsubst < k8s-secret.yml.yml > k8s-secret.yml.yml
-kubectl apply -f k8s-configMap.yml
-kubectl apply -f k8s-secret.yml
+envsubst < k8s-configMap.yml > k8s-configMapOut.yml
+envsubst < k8s-secret.yml.yml > k8s-secretOut.yml
+kubectl apply -f k8s-configMapOut.yml
+kubectl apply -f k8s-secretOut.yml
 kubectl apply -f k8s-developments.yml
 kubectl apply -f k8s-service.yml
